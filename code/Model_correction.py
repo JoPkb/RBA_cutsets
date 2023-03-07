b901d48787439858a0e8bf5f5aa659ea52caf8d8
JoPkb <jeremiemprokob@gmail.com>
2023-03-07 16:59:01 +0100

    Petit rangement du git

---
+++ code/Model_correction.py
@@ -0,0 +1,133 @@
+import cobra
+from datetime import datetime
+import requests
+import json
+from tqdm import tqdm
+
+### Function to copy-paste genes from one model to another :
+
+def copy_genes(origin_model, target_model) :
+    
+    for target_reaction in target_model.reactions :
+        try :
+            target_reaction.gene_reaction_rule = origin_model.reactions.get_by_id(target_reaction.id).gene_reaction_rule
+            print(f"\nDEBUG -- Added gene_reaction_rule to reaction {target_reaction.id}")
+        except KeyError :
+            print(f"\nERROR -- KeyError, The reaction from target model {target_reaction.id} might not have a counterpart in origin_model.")
+    return target_model
+
+def fix_formulas(model) :
+
+    for m in model.metabolites :
+       
+        m.formula = ""
+    
+    return model
+
+
+### Gene id conversion :
+def get_ids(model, external_db, annotation_key) :
+    url="http://rest.ensembl.org/xrefs/id/"
+    #print("\nCreating working copy of the model...")
+    #converted = model.copy()
+    #print("\nModel copied.")
+    error_messages = []
+    if len(model.genes) == 0 :
+        print(f"\nERROR -- No genes in model {model.name}")
+        return 0
+    for gene in tqdm(model.genes) :
+        gene_id = gene.id
+        #print(f"Checking out gene {gene_id}")
+        # Checking if the gene id is an ENSEMBL id :
+        if len(gene_id) == 15 :
+
+            #print(f"\nDEBUG : getting ncbi gene id for {gene_id}")
+            response = requests.get(url+gene_id+"?external_db="+external_db)
+
+            try :
+
+                # Parsing of the ncbi gene ID from the text response
+                new_id = response.text.split("primary_id: ")[1].split("\n")[0]
+                gene.annotation[annotation_key] = new_id
+                #print(f"\nAdded ncbi gene id {new_id} to annotations of gene {gene_id}")
+
+            except IndexError :
+                    current_time = datetime.now().strftime("%H:%M:%S")
+                    error_messages.append(f"\n[{current_time}] ERROR - No ncbi gene id found for gene {gene_id}.")
+                    
+                    pass
+        else :
+            current_time = datetime.now().strftime("%H:%M:%S")
+            error_messages.append(f"\n[{current_time}]ERROR - Gene id {gene_id} is not an ENSEMBL id.")
+            
+    for message in error_messages :
+        print(message)
+    """
+    for reaction in converted.reactions :
+        print(f"\nChanging gene_reaction_rule for reaction {reaction.id}")
+        reaction_genes = list(reaction.genes)
+        string = ""
+        for gene_name in reaction_genes :
+            if reaction_genes.index(gene_name) != len(reaction_genes)-1 :
+                string += f"{gene_name} or "
+            else :
+                string += f"{gene_name}"
+        reaction.gene_reaction_rule = string
+        #reaction.gene_name_reaction_rule = string"""
+
+    #return converted
+
+
+def get_subsystem(source_model, target_model) :
+    for source_reaction in source_model.reactions :
+        source_reaction_id = source_reaction.id
+
+        if len(source_reaction.annotation) != 0 :
+            print(f"\nGetting subsystem information from source model for reaction {source_reaction_id}")
+            target_model.reactions.get_by_id(source_reaction_id).subsystem = source_reaction.subsystem
+            
+        else :
+            print(f"\nNo subsystem found for reactions {source_reaction_id}")
+"""
+### Function to add biomass_reaction :
+def add_biomass_reaction(model, biomass_metabolites) :
+    #biomass_metabolites = './Hep-G2/Hep-G2.xml-5a91f1955e8a9c8a5c5d2ff4a1737c21/utils_biomass_metabolites.txt'
+    biomass_components_manual = []
+    with open(biomass_metabolites, 'r') as input_metabolites :
+        metabolites_list = input_metabolites.readlines()
+        
+        for metabolite_to_add in metabolites_list :
+            biomass_components_manual.append(metabolite_to_add.strip('\n'))
+    reaction = cobra.Reaction('artificial_biomass')
+    reaction.lower_bound = 0.0
+    reaction.upper_bound = 1000.0
+    temp001x= cobra.Metabolite(
+        'temp001x',
+        formula='',
+        name='biomass_',
+        compartment='C_c')
+    #Ajout de la sortie de la biomasse
+    output_biom = cobra.Reaction('EX_temp001x')
+    output_biom.lower_bound = 0.0
+    output_biom.upper_bound = float("inf")
+    output_biom.add_metabolites({temp001x :-1.0})
+    model.add_reactions([output_biom])
+    model.add_reactions([reaction])
+    model.reactions.artificial_biomass.add_metabolites({temp001x : 1.0})
+    for metabolite in biomass_components_manual :
+        model.reactions.artificial_biomass.add_metabolites({model.metabolites.get_by_id(str(metabolite)) : -1.0})
+    return model
+def remove_gene_dupes(json_in, json_out) :
+    with open(json_in) as input_file :
+        data = json.load(input_file)
+    unique_ids = []
+    for g in data["genes"] :
+        id = g["id"]
+        if id not in unique_ids :
+            unique_ids.append(id)
+        else : 
+            print(f"{id} already found somewhere else.")
+    data["genes"] = [{"id" : id, "name" : ""} for id in unique_ids]
+    with open(json_out, "w") as output_file :
+        json.dump(data, output_file, indent="")
+"""
---
+++ code/model_maj.py
@@ -0,0 +1,125 @@
+import cobra
+from cobra.io import validate_sbml_model
+import Model_correction as mc
+import cbmpy
+import sys
+import importlib
+import os
+
+importlib.reload(mc)
+def maj(source_model, target_model, dir, bounds_check =True, genes_id_copy = True, alt_gene_ids = True, metab_id_check = True, bounds_value_check = True, subsystem_copy = True, exchanges_copy = True) :
+
+    print("\nLoading model to update...\n")
+    target_model_file_name = target_model.split(".xml")[0]
+    
+
+    # If all bounds are the same, load the model with 
+    if bounds_check :
+        print("\nLoading with cbmpy")
+        if not os.path.exists(dir+target_model_file_name+"_cbmpy.xml") :
+            target_model_temp = cbmpy.CBRead.readSBML2FBA(dir + target_model)
+            cbmpy.CBWrite.writeSBML3FBCV2(target_model_temp, dir+target_model_file_name+"_cbmpy.xml")
+            del(target_model_temp)
+            working_target_model, errors = validate_sbml_model(dir+target_model_file_name+"_cbmpy.xml")
+        else :
+            working_target_model, errors = validate_sbml_model(dir+target_model_file_name+"_cbmpy.xml")
+
+        print("Cobra model loading errors :\n")
+        print(errors["COBRA_ERROR"])
+    # ^ Maybe not needed : can just use target_model_temp ? 
+    else :
+
+        working_target_model, errors = validate_sbml_model(dir+target_model)
+        print("Cobra model loading errors :\n")
+        print(errors["COBRA_ERROR"])
+    
+    if genes_id_copy or subsystem_copy:
+        print("\nLoading source model for genes copy : \n")
+        source_model_cobra, errors_source = validate_sbml_model(dir+source_model)
+    else :
+        pass
+
+    if genes_id_copy :
+        # TEST # if no genes in model.genes : (else, dont do it)
+        print("\nCopying genes from model model to target model : \n")
+        working_target_model = mc.copy_genes(source_model_cobra, working_target_model)
+    else :
+        pass
+    
+    if "m01602c" in [m.id for m in working_target_model.reactions.biomass_components.metabolites] :
+        working_target_model.reactions.biomass_components.add_metabolites({"m01602c" : 0.0}, combine=False)
+    
+    else :
+        pass
+    
+    if alt_gene_ids :
+        # TEST # if gene.annotation exists : (else : dont)
+        # THEN TEST # if gene.annotation["ensembl"] and/or gene.annotation["ncbigene"] are present
+        print("\nGetting ENSEMBL and ncbi genes IDs to the annotations of the target model : \n")
+        for gene in working_target_model.genes :
+            if len(gene.id) == 15 and "ENSG" in gene.id[:4] :
+                gene.annotation["ensembl"] = str(gene.id)
+        
+        mc.get_ids(working_target_model, "EntrezGene", "ncbigene")
+    else :
+        pass
+
+    if metab_id_check :
+        print("\nCleaning up the metabolites list in the target model : \n")
+        to_remove = []
+        for metabolite in working_target_model.metabolites :
+            if "m" not in metabolite.id :
+                to_remove.append(metabolite)
+        working_target_model.remove_metabolites(to_remove)
+    else :
+        pass
+    
+    if bounds_value_check :
+        print(f"\nNumber of metabolites in target model : {len(working_target_model.metabolites)}")
+        print("\nSetting the reactions to finite bounds in the target model : \n")
+
+        for reaction in working_target_model.reactions :
+            ub = 1000.0 if reaction.upper_bound == float("inf") or reaction.upper_bound == 1000.0 else 0.0
+            lb = -1000.0 if reaction.lower_bound == float("-inf") or reaction.lower_bound == -1000.0 else 0.0
+            #print(f"DEBUG --- reaction {reaction.id}\noriginal bounds : ({reaction.lower_bound},{reaction.upper_bound})\nnew bounds : ({lb},{ub})\n\t########\n")
+            reaction.bounds = (lb,ub)
+    else :
+        pass
+
+    if subsystem_copy :
+        print(f"\nGetting subsystem information from source model.")
+        mc.get_subsystem(source_model_cobra, working_target_model)
+    else :
+        pass
+
+    print("\n\nDONE\n\n")
+
+
+    if exchanges_copy :
+        print(f"\nCopying exchange reactions from source model.")
+        
+
+    return working_target_model
+
+if __name__ == "__main__" :
+    converted_target_model = maj(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    cobra.io.write_sbml_model(converted_target_model, sys.argv[3].split(".xml")[0] + "_updated.xml")
+
+
+
+
+"""
+MISC
+
+    # TEST # if all bounds are te same : same_bounds = True, else False
+    same_bounds = True
+    for i in range(len(target_model.reactions)) :
+        if i == 0 :
+            bound_to_compare = target_model.reactions[i].bounds
+        else :
+            if bound_to_compare != target_model.reactions[i].bounds :
+                same_bounds = False
+
+
+"""
