036ce0d6187153136db178898b7bf61ad94f4ca5
JoPkb <jeremiemprokob@gmail.com>
2023-04-07 10:53:15 +0200

    Ajout des scripts d'application du medium au fichier Model_correction.py

--- code/utils/Model_correction.py
+++ code/utils/Model_correction.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import requests
 import json
 from tqdm import tqdm
+from cobra.core.model import Model
+from cobra.core.reaction import Reaction
 
 ### Function to copy-paste genes from one model to another :
 
@@ -152,6 +154,7 @@ def get_exchanges_reactions(target_model) :
             reactions_to_add.append(new_reaction_x)
     target_model.add_reactions(reactions_to_add)
 
+
 def check_biomass_metabolites(target_model, metabolite_id = "_") :
     print(f"\nChecking if the metabolite [cofactors and vitamins] is included in the biomass_components reaction of the model.")
 
@@ -163,10 +166,114 @@ def check_biomass_metabolites(target_model, metabolite_id = "_") :
             biomass_reaction.add_metabolites({target_model.metabolites.get_by_id(id) : 0.0}, combine=False)
             print(f"\nFound unwanted metabolite in biomass_components reactions : {id}. Removed it.")
 
+###- MEDIUM DEFINITION AND APPLICATION FUNCTIONS -###
 
-def get_medium(target_model, concentrations) :
 
-    pass
+def export_medium(model:Model, outfile:str):
+    """
+    Function exporting the cobra metabolic model's medium
+    in a tabular text file with semicolon separators.
+    Used to manually specify which intakes the model takes.
+    Type 1 at the end of a line to include that reaction, 
+    leave empty to leave it out.
+    """
+    str = ""
+    for exchange_reaction in model.medium:
+        reaction = model.reactions.get_by_id(exchange_reaction)
+        metab = [m for m in reaction.metabolites.keys()][0]
+        str += f"\n{reaction.id};{metab.name};"
+    
+    with open(outfile, "w") as out_medium :
+        out_medium.write(str)
+
+
+def parse_medium_definition(file_path:str, model:Model):
+    """
+    Function parsing the tabular text file defining the medium
+    for the cobra metabolic model. Each line with a "1" at the end 
+    will get included in the "reactions to keep" list, the others will
+    not.
+    Returns a tuple of two lists : exchange reactions to remove, exchange reactions to keep.
+    """
+    to_remove = []
+    to_keep = []
+    model.objective = "biomass_components"
+    with open(file_path, "r") as medium_data:
+        medium = medium_data.read()
+
+    for line in medium.split("\n")[1:]:
+        reaction_id = line.split(";")[0]
+        reaction_presence = line.split(";")[2]
+
+        if reaction_presence == "1":
+
+            reaction_to_keep = model.reactions.get_by_id(reaction_id)
+            m = [m.name for m in reaction_to_keep.metabolites][0]
+            # print(f"\n{reaction_id} -- {m} is present in medium")
+            to_keep.append(reaction_id)
+
+        else:
+
+            reaction_to_remove = model.reactions.get_by_id(reaction_id)
+            m = [m.name for m in reaction_to_remove.metabolites][0]
+            # print(f"\nReaction {reaction_id} -- {m} not in medium.")
+            to_remove.append(reaction_id)
+
+    return (to_remove, to_keep)
+
+
+def change_reaction_bounds(model: Model, reactions: dict[str:tuple[float, float]]):
+    """
+    Function used to cut one or more reactions in a cobra metabolic model.
+    Takes as input a cobra model object and a dictionary containing reaction
+    IDs and their desired new bounds.
+    """
+    for reaction_id, bounds in reactions.items():
+        reaction_to_change = model.reactions.get_by_id(reaction_id)
+        reaction_to_change.bounds = bounds
+
+
+def apply_medium_safe(model:Model, to_remove:list[str], objective_function:str, threshold:float):
+    """
+    Function used to apply the medium constraint on the model
+    given as input, from the list of reactions to remove given as input.
+    The list should contain strings of reactions IDs from the model.
+    The function removes the reactions one by one, while checking that
+    the objective function of the model stays solvable. If the objective_value
+    decreases by more than the set threshold given as input, then the reaction 
+    will be restored.
+    The function returns a tuple of the modified model and 
+    a list of all the reaction IDs that have been restored.
+    """
+    #Creating a copy of the model
+    modified_model = model.copy()
+
+    modified_model.objective = objective_function
+    objective_value = modified_model.optimize().objective_value
+    #For each reaction, we cut it, check the biomass production, then, if it does not
+    #respect our criteria, it is restored.
+    reactions_restored = []
+
+    for reaction_id in to_remove:
+        rdict = {reaction_id : (0.0,1000.0)}
+        change_reaction_bounds(modified_model, rdict)
+        obj_val_tmp = modified_model.optimize().objective_value
+        rdict_restore = {reaction_id : (-1000.0,1000.0)} 
+
+        if obj_val_tmp == 0 :
+            print(f"\nWhen cutting reaction {reaction_id}, biomass fell to 0.")
+            change_reaction_bounds(modified_model, rdict_restore)
+            reactions_restored.append(reaction_id)
+
+        elif objective_value - obj_val_tmp > threshold:
+            print(f"\nWhen cutting reaction {reaction_id}, biomass was reduced by {objective_value-obj_val_tmp}")
+            change_reaction_bounds(modified_model, rdict_restore)
+            reactions_restored.append(reaction_id)
+
+        else:
+            print(f"Minimal consequences when cutting reaction {reaction_id}, biomass changed by {objective_value-obj_val_tmp}")
+    print(f"\nMedium definition finished. Removed {len(to_remove)-len(reactions_restored)} reactions out of {len(to_remove)}")
+    return modified_model, reactions_restored
 
 
 def get_reac_id_from_genes(target_model, source_model):
