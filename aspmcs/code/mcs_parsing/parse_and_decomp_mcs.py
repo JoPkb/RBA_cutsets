f67946136033c08f0302c0e7edc725ffc4b44309
JoPkb <muller-prokob@T14s-muller-prokob.bioinf.uni-duesseldorf.de>
2024-05-17 17:37:55 +0200

    Modification of the parsing and decompression script to adapt for the new way of running aspefm with multiple positive constraints

--- aspmcs/code/mcs_parsing/parse_and_decomp_mcs.py
+++ aspmcs/code/mcs_parsing/parse_and_decomp_mcs.py
@@ -9,7 +9,7 @@ import pickle
 def itertools_product(listofcutsets, model, objective="biomass_components"):
     i = 1
     good_cutsets = []
-    for cutset_orID in tqdm(listofcutsets):
+    for cutset_orID in listofcutsets:
         good_cutsets_subs = []
         #print(f"Cutset : {cutset_orID}\n")
         cuts_combinations = list(product(*cutset_orID)) # Décompression des réactions retrouvées dans les cutsets
@@ -23,16 +23,21 @@ def itertools_product(listofcutsets, model, objective="biomass_components"):
             if retval < 1e-6:
                 good_cutsets_subs.append(comb)
             else:
-                print(f"{comb} is not a cutset.")
+                print(f"[!] {comb} is not a cutset.")
 
             if len(comb) > 1 :
                 for nm1 in combinations(comb, len(comb)-1):
                     retval = sol_after_ko(nm1, m)
                     if retval < 1e-6:
-                        print('[N-1]',"Objective value = ", retval,"For n-1 subset\
+                        print('[!] [N-1]',"Objective value = ", retval,"For n-1 subset\
                            of cutset >>>", nm1, 'not MIN cutset')
-        good_cutsets.append(good_cutsets_subs)
-        i+=1
+        
+        if len(good_cutsets_subs) > 0:
+            print(f"[>] Adding {good_cutsets_subs} to mcs list.")
+            good_cutsets.append(good_cutsets_subs)
+            i+=1
+        else:
+            print(f"[!] MCS {cutset_orID} has no valid counterpart in the decompressed model.")
     return good_cutsets
 
 if __name__ == "__main__":
@@ -41,22 +46,25 @@ if __name__ == "__main__":
     reaction_subsets = sys.argv[3]
     output_path = sys.argv[4]
     model = cobra.io.read_sbml_model(model_path)
-    cutsets_list = parse_output(cutsets_path)
+    posconstr2cutset = parse_output(cutsets_path)
     with open(reaction_subsets, 'r') as buffer_subsets:
         reaction_subsets_dict = eval(buffer_subsets.read())
     listofcutsets_original_id = []
-    for reac_list in cutsets_list:
-        #Pour chaque cutset:
+    posconstr2decompmcs = {}
+    for posconstr, cutsets_list in posconstr2cutset.items():
+        for reac_list in cutsets_list:
+            #Pour chaque cutset liés à une posconstr:
 
-        cutset_original_IDs = []
-        #print(f"\n\nFor cutset {reac_list}:\n\n")
-        for comp_reac_id in reac_list:
-            #Pour chaque réaction compressée du cutset
-            #On veut récupérer le subset de réactions pré-compression.
-            reac_subset = reaction_subsets_dict[comp_reac_id[4:]]["reacs"]
-            cutset_original_IDs.append(reac_subset)
-        listofcutsets_original_id.append(cutset_original_IDs)
-    
-    good_cutsets = itertools_product(listofcutsets_original_id, model)
+            cutset_original_IDs = []
+            #print(f"\n\nFor cutset {reac_list}:\n\n")
+            for comp_reac_id in reac_list:
+                #Pour chaque réaction compressée du cutset
+                #On veut récupérer le subset de réactions pré-compression.
+                reac_subset = reaction_subsets_dict[comp_reac_id[4:]]["reacs"]
+                cutset_original_IDs.append(reac_subset)
+            listofcutsets_original_id.append(cutset_original_IDs)
+
+        posconstr2decompmcs[posconstr] = itertools_product(listofcutsets_original_id, model)
+        
     with open(output_path, "wb") as out:
-        pickle.dump(good_cutsets,out)
+        pickle.dump(posconstr2decompmcs,out)
