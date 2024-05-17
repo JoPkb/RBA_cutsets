e8050cc65dfb57c3ab6dbf17f3dcd6f5b76433ed
JoPkb <muller-prokob@T14s-muller-prokob.bioinf.uni-duesseldorf.de>
2024-05-17 17:36:44 +0200

    Addition of a function to get mcs counts (per file, per size, and others) to the utils functions

--- aspmcs/code/mcs_parsing/utils.py
+++ aspmcs/code/mcs_parsing/utils.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 import warnings
+import os
 def knock_out(model, lr):
     model = model.copy()
     for r in lr:
@@ -66,6 +67,52 @@ def parse_output(path):
             cutset_list.append([rid.split("\")")[0] for rid in reacs])
     return cutset_list
 
+
+def get_mcs_counters(mcs_directory, output_sorting) :
+    """
+    Getting counter dictionaries of MCS results.
+    returns 3 dictionaries of MCS counts : 
+    overall_dMCS_sizes : counter of the length of MCS over all positive constraints runs
+        Note : this dictionary effectively contains the length distribution of compressed mcs.
+    per_file_dMCS_sizes : counter of the length of MCS for each individual positive constraint run
+    per_file_dMCS : Dictionary linking the name of the positive constraint and the list of MCS associated to it.
+    """
+    #mcs_directory="../../../HCC_compressed_2/mcs_posconstr_analysis_2/mcs_decomp/"
+
+    mcs_file_list = os.listdir(mcs_directory)
+    overall_MCS_sizes = {}
+    per_file_dMCS_sizes= {}
+    per_file_dMCS = {}
+    per_file_sat = {}
+    per_constraint_sat = {}
+    with open(output_sorting) as sat_file:
+        sat_lines = sat_file.readlines()
+        for line in sat_lines:
+            mcs_filename = line.split(": ")[-1].strip("\n")
+            sat = line.split(" file :")[0]
+            per_file_sat[mcs_filename] = sat
+        
+
+    for mcs_file in mcs_file_list:
+        with open(f"{mcs_directory}{mcs_file}", "rb") as mcs_in:
+            mcs_list = pickle.load(mcs_in)
+            if len(mcs_list) > 0:
+                dMCS_sizes = {}
+                for mcs in mcs_list:
+                    dMCS_sizes[len(mcs[0])] = dMCS_sizes.get(len(mcs[0]), 0 ) + 1
+                    overall_MCS_sizes[len(mcs[0])] = overall_MCS_sizes.get(len(mcs[0]), 0) + 1
+                
+                mcs_log_file = mcs_file.split("_decomp")[0] + ".txt"
+                with open("../../../HCC_compressed_2/mcs_posconstr_analysis_2/mcs_res/"+mcs_log_file, "r") as mcs_log:
+                    posconstr_line = mcs_log.readlines()[0]
+                    posconstr = posconstr_line.split('support("')[-1].split('")')[0]
+                try:
+                    per_constraint_sat[posconstr] = per_file_sat[mcs_log_file]
+                except KeyError:
+                    per_constraint_sat[posconstr] = "NA"
+                per_file_dMCS_sizes[posconstr] = dMCS_sizes
+                per_file_dMCS[posconstr] = mcs_list
+    return overall_MCS_sizes, per_file_dMCS_sizes, per_file_dMCS, per_file_sat, per_constraint_sat
 # Ne fonctionne pas. Utiliser la fonction itertools_products de parse_and_decomp_mcs.py
 # def decompress_cutsets(subsets_file_path, comp_cutsets_list):
 #     with open(subsets_file_path, 'r') as buffer_subsets:
