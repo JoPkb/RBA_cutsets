e684c550f3d2330d5dfb8ab3a01722a16b523fba
feq95hoc <jeremiemprokob@gmail.com>
2025-09-04 17:23:58 +0200

    Documentation test 1

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+# Configuration file for Sphinx
+
+project = "My Project"
+author = "Your Name"
+
+extensions = [
+    "myst_nb",  # enables notebook support
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "alabaster"  # or "sphinx_rtd_theme" if you prefer
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+Welcome to My Project’s Documentation
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   ../notebooks/example.ipynb
